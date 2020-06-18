0ad38dd5ec6927f2b8f4795c3f5355ed61ba1d5d
Everton Mello <evertonmello@brq.com>
2020-06-18 17:06:33 -0300

    second version

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,
+  ViewChild,
+  ElementRef } from '@angular/core';
 
 import { AppService } from './../app.service'
 
@@ -8,9 +10,12 @@ import { AppService } from './../app.service'
   styleUrls: ['./schedule.component.scss']
 })
 export class ScheduleComponent implements OnInit {
+  @ViewChild('timesCtn',{static: false}) timesCtn: ElementRef;
+  @ViewChild('hdrTimeCtn',{static: false}) hdrTimeCtn: ElementRef;
 
   showMore = false;
-  dates;
+  dates = [];
+  scheduleIndex = 0;
   constructor(private appService:AppService) {}
 
   ngOnInit(){
@@ -19,12 +24,39 @@ export class ScheduleComponent implements OnInit {
 
 
   getTimes(){
-    this.appService.getTimes().subscribe((times)=>{
-      this.dates = times
+    this.appService.getTimes().subscribe((times:Array<any>)=>{
+      this.dates = times;
     },(error)=>{
       alert('Ops, ocorreu um erro' + error.message)
     })
   }
+
+  next(){
+    this.scheduleIndex++
+    this.timesCtn.nativeElement.style.transform = "translate(-" + this.scheduleIndex * 240 +"px)"
+    this.hdrTimeCtn.nativeElement.style.transform = "translate(-" + this.scheduleIndex * 240 +"px)"
+  }
+
+  prev(){
+    this.scheduleIndex--
+    this.timesCtn.nativeElement.style.transform = "translate(-" + this.scheduleIndex * 240 +"px)"
+    this.hdrTimeCtn.nativeElement.style.transform = "translate(-" + this.scheduleIndex * 240 +"px)"
+  }
+  dsblPrev(){
+    if(this.scheduleIndex == 0){
+      return true;
+    }
+  }
+  dsblNext(){
+    if(this.scheduleIndex == Math.floor(this.dates.length/4)){
+      return true;
+    }
+  }
+
+  changeTimes(){
+    this.showMore = !this.showMore;
+  }
+  
 
   dayOfWeek(strDate){
     let date = new Date(strDate);
